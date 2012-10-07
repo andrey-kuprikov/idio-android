8b1222843b6cb4c911319915f35fc403728fc993
Andrey Kuprikov <andrey.kuprikov@gmail.com>
2012-10-07 18:09:55 +0400

    some layout experiments

--- com.example.test.MainActivity/src/com/example/test/RestClient.java
+++ com.example.test.MainActivity/src/com/example/test/RestClient.java
@@ -7,12 +7,20 @@ public class RestClient {
 
 	private static AsyncHttpClient client = new AsyncHttpClient();
 
-	public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+	public static void get(String url, RequestParams params, AsyncHttpResponseHandler<?> responseHandler) {
 		client.get(getAbsoluteUrl(url), params, responseHandler);
 	}
 
-	public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+	public static void post(String url, RequestParams params, AsyncHttpResponseHandler<?> responseHandler) {
 		client.post(getAbsoluteUrl(url), params, responseHandler);
+	}
+
+	public static void put(String url, RequestParams params, AsyncHttpResponseHandler<?> responseHandler) {
+		client.put(getAbsoluteUrl(url), params, responseHandler);
+	}
+
+	public static void delete(String url,AsyncHttpResponseHandler<?> responseHandler) {
+		client.delete(getAbsoluteUrl(url), responseHandler);
 	}
 
 	private static String getAbsoluteUrl(String relativeUrl) {
